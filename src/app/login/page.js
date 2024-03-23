1893846586b65e27ec5fba7193abf0c105ff6b30
SilviaAsmat <silvia.asmat@g.austincc.edu>
2024-03-23 14:34:17 -0500

    Created login page

---
+++ src/app/login/page.js
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription } from "@/components/ui/card";
+import { Tabs } from "@radix-ui/react-tabs";
+
+
+
+export default function Home() {
+    return (
+        //<main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-br from-purple-400 via-pink-500 to-red-500">
+            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                    Welcome. Are you ready to race to save?&nbsp;
+
+                </p>
+                <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+                    <a
+                        className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+                        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        By{" "}
+                        <Image
+                            src="/traced-hadoopheros.png"
+                            alt="Vercel Logo"
+                            //className="dark:invert"
+                            width={100}
+                            height={24}
+                            priority
+                        />
+                    </a>
+                </div>
+
+                <CardWithForm></CardWithForm>
+
+            </div>
+
+
+
+
+
+        </main>
+    );
+}
